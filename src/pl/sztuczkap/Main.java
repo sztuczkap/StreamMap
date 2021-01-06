b5db606464ba8c97136fe6526688c1a0d0a723ee
sztuczkap <p.sztuczka@wp.pl>
2021-01-06 15:26:38 +0100

    Tworzenie struktury Map

--- src/pl/sztuczkap/Main.java
+++ src/pl/sztuczkap/Main.java
@@ -1,8 +1,70 @@
 package pl.sztuczkap;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        List<User> users = new ArrayList<>();
+        users.add(new User(1, "Jan", 30, List.of("Sport", "Motoryzacja")));
+        users.add(new User(2, "Ola", 20, List.of("Film", "Podróże")));
+        users.add(new User(3, "Adam", 25, List.of("Informatyka")));
+        users.add(new User(4, "Jan", 50, List.of("Malarstwo")));
+
+/*        // z list tworzymy mape jako klucz to id a wartosc imie
+        Map<Integer, String> collect =  users
+                .stream()
+                .collect(Collectors.toMap(User::getId, User::getName));*/
+
+/*        // mapa: id jako klucz i obiekt jako wartość mapy
+        Map<Integer, User> collect = users
+                .stream()
+                .collect(Collectors.toMap(User::getId, Function.identity()));  // value: user -> user*/
+
+/*        // mapa: klucz imie; wartośc: wiek użytkownika
+        Map<String, Integer> collect = users.stream()
+                .collect(Collectors.toMap( // Exception: Duplicate key Jan (przed przeciążeniem)
+                        User::getName,
+                        User::getAge,
+                        (oldValue, newValue) -> newValue)); // przeciążenie metody toMap
+
+        collect.forEach((k, v) -> System.out.println("Key: " + k + " value: " + v));*/
+
+/*        // mapa imion z przypisanymi zainteresowaniami
+        // w przypadku tych samych kluczy połączymy je w pojedyncza liste
+        Map<String, List<String>> collect = users.stream()
+                .collect(Collectors.toMap(
+                        User::getName,
+                        User::getHobbies,
+                        (oldValue, newValue) ->
+                                Stream.concat(oldValue.stream(), newValue.stream())
+                                        .collect(Collectors.toList())
+                ));
+        // polaczone zainteresowania  usytkownika JAN
+        collect.forEach((k, v) -> System.out.println("Key: " + k + " value: " + v));*/
+
+
+        // 4 argument który ma jedna z przeciazonych wersji toMap
+        // podajemy referencje do konstruktora i wymuszamy jaka struktura zostanie stworzona
+        // i w tym przypadku tworzymy TreeMap
+        TreeMap<String, List<String>> collect = users.stream()
+                .collect(Collectors.toMap(
+                        User::getName,
+                        User::getHobbies,
+                        (oldValue, newValue) ->
+                                Stream.concat(oldValue.stream(), newValue.stream())
+                                        .collect(Collectors.toList()),
+                        TreeMap::new
+                ));
+        // polaczone zainteresowania  usytkownika JAN  + posortowane klucze
+        collect.forEach((k, v) -> System.out.println("Key: " + k + " value: " + v));
+
     }
 }
---
+++ src/pl/sztuczkap/User.java
@@ -0,0 +1,56 @@
+package pl.sztuczkap;
+
+import java.util.List;
+
+public class User {
+
+    private Integer id;
+    private String name;
+    private Integer age;
+    private List<String> hobbies;
+
+    public User(Integer id, String name, Integer age, List<String> hobbies) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.hobbies = hobbies;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public List<String> getHobbies() {
+        return hobbies;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        if (name != null ? !name.equals(user.name) : user.name != null) return false;
+        if (age != null ? !age.equals(user.age) : user.age != null) return false;
+        return hobbies != null ? hobbies.equals(user.hobbies) : user.hobbies == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (age != null ? age.hashCode() : 0);
+        result = 31 * result + (hobbies != null ? hobbies.hashCode() : 0);
+        return result;
+    }
+}
